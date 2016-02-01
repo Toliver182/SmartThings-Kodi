a06cbb811ef9e2fea4bb528158f6c26afd251596
Oliver Kelly <tkfun2@me.com>
2016-02-01 15:51:18 +0000

    updated info

--- devicetypes/toliver182/kodi-client.src/kodi-client.groovy
+++ devicetypes/toliver182/kodi-client.src/kodi-client.groovy
@@ -1,6 +1,6 @@
 /**
  * 
- *  Copyright 2016 Tolvier182
+ * forked from a pelx version: https://github.com/iBeech/SmartThings/tree/master/PlexManager
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
